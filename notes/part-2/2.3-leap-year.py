075fd0b772aaf4c9fbf44a5663981c70f330f7aa
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-11-21 17:06:14 +0800

    Add leap year checker program
    Implement a program to check if a year is a leap year based on user input.

---
+++ notes/part-2/2.3-leap-year.py
@@ -0,0 +1,32 @@
+## Generally, any year that is divisible by four is a leap year.
+## However, if the year is additionally divisible by 100, it is a leap year only if it also divisible by 400.
+
+## Please write a program which asks the user for a year, and then prints out whether that year is a leap year or not.
+
+## Some examples:
+
+## Sample output
+# Please type in a year: 2011
+# That year is not a leap year.
+
+## Sample output
+# Please type in a year: 2020
+# That year is a leap year.
+
+## Sample output
+# Please type in a year: 1800
+# That year is not a leap year.
+
+
+## Solution:
+year = int(input("Please type in a year:"))
+
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("That year is a leap year.")
+    else:
+        print("That year is not a leap year.")
+elif year % 4 == 0:
+    print("That year is a leap year.")
+else:
+    print("That year is not a leap year.")
