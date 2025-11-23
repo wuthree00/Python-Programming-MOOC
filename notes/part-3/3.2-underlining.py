924cfb015355ec442ac71a26da7047acdb506835
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-11-23 19:03:01 +0800

    Create 3.2-underlining.py

---
+++ notes/part-3/3.2-underlining.py
@@ -0,0 +1,31 @@
+## Please write a program which asks the user for strings using a loop.
+## The program prints out each string underlined as shown in the examples below.
+## The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
+
+## Sample output
+# Please type in a string: Hi there!
+
+# Hi there!
+# ---------
+
+# Please type in a string: This is a test
+
+# This is a test
+# --------------
+
+# Please type in a string: a
+
+# a
+# -
+
+# Please type in a string:
+
+
+## Solution
+string = input("Please type in a string:")
+
+while string != "":
+    count = len(string) #this is an integer
+    print(string)
+    print(count*"-")
+    string = input("Please type in a string:")
