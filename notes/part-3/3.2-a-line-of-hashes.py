146afe5c09d72f7de0e9f5bac6294a871a99e561
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-11-23 19:00:55 +0800

    Create 3.2-a-line-of-hashes.py

---
+++ notes/part-3/3.2-a-line-of-hashes.py
@@ -0,0 +1,15 @@
+## Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+# Note that the single apostrophes are not printed out by the programme but are used to distinguish the # marking the comments and the printed output.
+
+## Sample output
+Width: 3
+# '###'
+
+
+## Sample output
+# Width: 8
+# '########'
+
+
+## Solution
+print(width*"#")
