367499870374f1450b85bf214e9aac9811bc24be
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-12-27 15:17:19 +0800

    Create 4.2-line.py

---
+++ notes/part-4/4.2-line.py
@@ -0,0 +1,27 @@
+## Please write a function named line, which takes two arguments: an integer and a string.
+## The function prints out a line of text, the length of which is specified by the first argument.
+## The character used to draw the line should be the first character in the second argument.
+## If the second argument is an empty string, the line should consist of stars.
+
+# An example of expected behaviour:
+# line(7, "%")
+# line(10, "LOL")
+# line(3, "")
+
+
+## Sample output
+# %%%%%%%
+# LLLLLLLLLL
+# ***
+
+
+## Solution:
+def line(a, b):
+    if b != "":
+        print(a*b[0])
+    else:
+        print(a*"*")
+
+#Test the function by calling it:
+if __name__ == "__main__":
+    line(7, "")
