ade417e6c9ccfb6cf545582b06b9c234f7b7e7c1
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-03-07 15:07:47 +0800

    Create 4.5-integers-to-strings.py

---
+++ notes/part-4/4.5-integers-to-strings.py
@@ -0,0 +1,39 @@
+## Please write a function named formatted, which takes a list of floating point numbers as its argument.
+## The function returns a new list, which contains each element of the original list in string format, rounded to two decimal points.
+## The order of the items in the list should remain unchanged.
+
+# Hint: use f-strings to format the floating point numbers into suitable strings.
+
+#------------------------------------------
+# An example of expected behaviour:
+# my_list = [1.234, 0.3333, 0.11111, 3.446]
+# new_list = formatted(my_list)
+# print(new_list)
+
+# Sample output
+# ['1.23', '0.33', '0.11', '3.45']
+#------------------------------------------
+
+
+## Solution:
+def formatted(my_list):
+    new_list = []
+    for i in my_list:
+        convert = f"{i:.2f}"
+        new_list.append(convert)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
+
+
+## Model solution: ##remove hashes##
+# def formatted(my_list: list):
+#     result = []
+ 
+#     for number in my_list:
+#         result.append(f"{number:.2f}")
+ 
+#     return result
