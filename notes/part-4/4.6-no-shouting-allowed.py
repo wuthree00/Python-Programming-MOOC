105824dc710cf673154e2c734d92d4e9433e8304
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-03-16 13:00:24 +0800

    Create 4.6-no-shouting-allowed.py

---
+++ notes/part-4/4.6-no-shouting-allowed.py
@@ -0,0 +1,41 @@
+## The Python string method isupper() returns True if a string consists of only uppercase characters.
+# Some examples:
+
+#-------------------------------
+# print("XYZ".isupper())
+# is_it_upper = "Abc".isupper()
+# print(is_it_upper)
+
+# Sample output
+# True
+# False
+#-------------------------------
+
+## Please use the isupper method to write a function named no_shouting, which takes a list of strings as an argument.
+## The function returns a new list, containing only those items from the original which do not consist of solely uppercase characters.
+
+# An example of expected behaviour:
+#--------------------------------------------------------------------------------------------
+# my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+# pruned_list = no_shouting(my_list)
+# print(pruned_list)
+
+# Sample output
+# ['def', 'lower', 'another lower', 'Capitalized']
+#--------------------------------------------------------------------------------------------
+
+
+## Solution:
+def no_shouting(my_list):
+    pruned_list = []
+    i = 0
+    for words in my_list:
+        if my_list[i].isupper() == False:
+            pruned_list.append(words)
+        i += 1
+    return pruned_list
+
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
