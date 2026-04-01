f3231c46d2cfa7b84fd88f54c4d814f325e22dda
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-04-01 14:34:30 +0800

    Create 5.1-number-of-matching-elements.py

---
+++ notes/part-5/5.1-number-of-matching-elements.py
@@ -0,0 +1,25 @@
+## Please write a function named count_matching_elements(my_matrix: list, element: int),
+## which takes a two-dimensional array of integers and a single integer value as its arguments.
+## The function then counts how many elements within the matrix match the argument value.
+
+# An example of how the function should work:
+# m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+# print(count_matching_elements(m, 1))
+
+# Sample output:
+# 3
+
+
+## Solution:
+def count_matching_elements(my_matrix: list, element: int):
+    count = 0
+    for row in my_matrix:
+        for item in row:
+            if item == element:
+                count += 1
+    return count
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
+  
