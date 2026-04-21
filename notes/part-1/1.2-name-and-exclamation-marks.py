33291a7a0c28e734fac34331f15012a79d36726f
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-04-21 19:03:53 +0800

    Update 1.2-name-and-exclamation-marks.py

--- notes/part-1/1.2-name-and-exclamation-marks.py
+++ notes/part-1/1.2-name-and-exclamation-marks.py
@@ -1,4 +1,6 @@
-## Please write a program which asks for the user's name and then prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, another between the two names and a third one at the end of the line.
+## Please write a program which asks for the user's name and then prints it out twice on a single line
+## so that there is an exclamation mark at the beginning of the line,
+## another between the two names and a third one at the end of the line.
 
 # The program should function as follows:
 
