ab3c353d6a217ccd04d2844b61c489e9701cd034
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-04-21 19:02:43 +0800

    Update 1.1-print-some-code.py

--- notes/part-1/1.1-print-some-code.py
+++ notes/part-1/1.1-print-some-code.py
@@ -1,4 +1,5 @@
-## Thus far, you have probably used double quotation marks " to print out strings. In addition to the double quotation marks, Python also accepts single quotation marks '.
+## Thus far, you have probably used double quotation marks " to print out strings.
+## In addition to the double quotation marks, Python also accepts single quotation marks '.
 
 ## This comes in handy if you ever want to print out the actual quotation marks themselves:
 # print('"Come right back!", shouted the police officer.')
