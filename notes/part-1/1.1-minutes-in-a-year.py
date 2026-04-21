ace4eb8cc014ff5e4b7067a690092d556b4317d5
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-04-21 19:03:00 +0800

    Refine comments in minutes calculation script
    Updated comments for clarity and formatting.

--- notes/part-1/1.1-minutes-in-a-year.py
+++ notes/part-1/1.1-minutes-in-a-year.py
@@ -1,4 +1,5 @@
-## Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
+## Please write a program which prints out the number of minutes in a year.
+## Use Python code to perform the calculation, as in the previous code example.
 
 ## solution:
 print(365*24*60)
