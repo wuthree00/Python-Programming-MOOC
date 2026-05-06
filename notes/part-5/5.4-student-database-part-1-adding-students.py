6bb951ccd76964f222e7dd4bbee94aa76afa2545
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2026-05-06 20:34:29 +0800

    Create 5.4-student-database-part-1-adding-students.py

---
+++ notes/part-5/5.4-student-database-part-1-adding-students.py
@@ -0,0 +1,48 @@
+### PART 1 ###
+
+## NOTE: This exercise contains 4 parts, with each subsequent part building on the code used in the previous part ##
+
+# I've separated my solution into 4 files corresponding with each part for easy reference and comparison of changes made to the functions defined in the solution
+
+
+## Adding students ##
+#---------------------------------------------------------------------------------------#
+## First write a function named add_student, which adds a new student to the database.
+## Also write a preliminary version of the function print_student,
+## which prints out the information of a single student.
+
+# These functions are used as follows:
+# students = {}
+# add_student(students, "Peter")
+# add_student(students, "Eliza")
+# print_student(students, "Peter")
+# print_student(students, "Eliza")
+# print_student(students, "Jack")
+
+# Your program should now print out:
+# Peter:
+#  no completed courses
+# Eliza:
+#  no completed courses
+# Jack: no such person in the database
+#---------------------------------------------------------------------------------------#
+
+
+## Solution:
+def add_student(students: dict, name: str):
+    students[name] = [] #add name to the dictionary called students, and set the value as an empty list (for use later)
+
+
+def print_student(students: dict, name: str):
+    if name in students:
+        print(f"{name}: \n no completed courses")
+    else:
+        print(f"{name}: no such person in the database")
+        
+if __name__ == "__main__":
+    students = {}
+    add_student(students, "Peter")
+    add_student(students, "Eliza")
+    print_student(students, "Peter")
+    print_student(students, "Eliza")
+    print_student(students, "Jack")
